c6bea08b64b822851a8a1895f3748cf6a2410205
Daniel <filhonando012@gmail.com>
2022-04-20 12:32:03 -0300

    Anotações

---
+++ js/main.js
@@ -0,0 +1,59 @@
+var d = new Date();
+alert(d.getMonth());
+
+
+
+/*var count
+for(count=0; count<=5; count++){
+    alert(count)
+}; */
+
+/*var count =0;
+while(count<=5){
+    console.log(count);
+    count++;
+};  */
+
+/*var idade= prompt("Qual a sua idade ");
+if (idade >18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade")
+};*/
+
+
+/*var fruta =[
+    {nome :"maça", cor:"vermelha"},
+    {nome :"uva", cor: "roxa"},
+    {nome : "banana", cor :"amarela"}
+] 
+console.log(fruta);
+alert(fruta[1].nome);*/
+
+
+
+/*
+var fruta ={nome :"Uva", cor:"vermelha"} 
+console.log(fruta.cor);
+alert(fruta.cor)*/
+
+
+/*lista.push("Uva")*/
+/*lista.pop()*/
+/*console.log(lista.length)*/
+/*console.log(lista[0])
+console.log(lista.toString()[0])*/
+/*console.log(lista.join("-")) */
+
+/*var nome =" Daniel Devid";
+var idade = 25;
+var frase = "Japão é o melhor time do mundo";
+
+
+
+//alert( idade + idade)//
+
+console.log(nome);
+console.log(idade*idade);
+console.log(frase.replace("Japão","Brasil"))
+alert(frase.replace("Japão","Brasil"))*/
